5c12bf41f7a8c48401c04a492ec1c815e75826d3
Pratik4230 <pratikjadhav1438@gmail.com>
2026-02-21 22:39:11 +0530

    Initalization

---
+++ src/app/api/chat+api.ts
@@ -0,0 +1,18 @@
+import { streamText, UIMessage, convertToModelMessages } from "ai";
+import { openai } from "@ai-sdk/openai";
+
+export async function POST(req: Request) {
+  const { messages }: { messages: UIMessage[] } = await req.json();
+
+  const result = streamText({
+    model: openai("gpt-5-mini"),
+    messages: await convertToModelMessages(messages),
+  });
+
+  return result.toUIMessageStreamResponse({
+    headers: {
+      "Content-Type": "application/octet-stream",
+      "Content-Encoding": "none",
+    },
+  });
+}
---
+++ src/utils/api.ts
@@ -0,0 +1,11 @@
+export const generateAPIUrl = (relativePath: string) => {
+  const path = relativePath.startsWith("/") ? relativePath : `/${relativePath}`;
+
+  if (!process.env.EXPO_PUBLIC_API_BASE_URL) {
+    throw new Error(
+      "EXPO_PUBLIC_API_BASE_URL environment variable is not defined",
+    );
+  }
+
+  return process.env.EXPO_PUBLIC_API_BASE_URL.concat(path);
+};
