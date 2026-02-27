283bebd98631d42d10d0da8d1546f03df94030bd
Pratik4230 <pratikjadhav1438@gmail.com>
2026-02-27 23:32:05 +0530

    icons

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,7 +1,1 @@
-export { Button } from "./Button";
-export { Input } from "./Input";
-export { Text } from "./Text";
-export { Card } from "./Card";
-export { Separator } from "./Separator";
-export { Spinner } from "./Spinner";
 export { SafeAreaView } from "./SafeAreaView";
